854ce41c3a6a439dbd32c50b04303b5300496338
Ilnaz <akpambekovilnaz@gmail.com>
2021-03-29 20:34:07 +0300

    models added

---
+++ src/express/models/search.js
@@ -0,0 +1,22 @@
+'use strict';
+
+const searchResultList = [
+  {
+    datetime: {
+      value: `2019-03-21T20:33`,
+      readableValue: `21.03.2019, 20:33`,
+    },
+    content: `Huawei открыла в России путешествия на смартфон Mate 30 Pro без сервисов Google`,
+  },
+  {
+    datetime: {
+      value: `2019-03-21T20:33`,
+      readableValue: `21.03.2019, 20:33`,
+    },
+    content: `«Яндекс.Метрика» запустила путешествия сервис для оценки эффективности баннеров и видеорекламы в реальном времени`,
+  },
+];
+
+module.exports = {
+  searchResultList,
+};
