4fa4763cf9f310867a5e780ba1b30b651b6647cf
Ilnaz <akpambekovilnaz@gmail.com>
2021-04-14 16:53:48 +0300

    review fix

--- src/service/cli/routes/posts.js
+++ src/service/cli/routes/posts.js
@@ -1,16 +1,20 @@
 'use strict';
 
 const path = require(`path`);
-const fs = require(`fs`);
-const {promises: fsPromises} = fs;
+const {promises: fsPromises} = require(`fs`);
 const {Router} = require(`express`);
+
 const myRouter = new Router();
+const DEFAULT_JSON_BODY = `[]`;
 
 myRouter.get(`/`, async (req, res) => {
   const articlesPath = path.join(__dirname, `../../../../mocks.json`);
-  const DEFAULT_JSON_BODY = `[]`;
-  const articles = fs.existsSync(articlesPath) ? await fsPromises.readFile(articlesPath, `utf-8`) : DEFAULT_JSON_BODY;
-  res.json(JSON.parse(articles || DEFAULT_JSON_BODY));
+  try {
+    const articles = await fsPromises.readFile(articlesPath, `utf-8`);
+    res.json(JSON.parse(articles || DEFAULT_JSON_BODY));
+  } catch (e) {
+    res.json(JSON.parse(DEFAULT_JSON_BODY));
+  }
 });
 
 module.exports = myRouter;
