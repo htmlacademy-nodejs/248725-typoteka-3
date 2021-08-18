c7089377880e26b7afcb6140b51717e801a9f6d7
Ilnaz <akpambekovilnaz@gmail.com>
2021-08-19 01:03:55 +0300

    categories calculation replaced with const one

--- src/service/api/categories.e2e.test.js
+++ src/service/api/categories.e2e.test.js
@@ -13,16 +13,21 @@ const app = express();
 app.use(express.json());
 registerCategoriesRoute(app, new DataService(mockData));
 
-const getAllCategories = (data) => {
-  const categoriesCollection = data.reduce((acc, item) => {
-    item.category.forEach((category) => acc.add(category));
-    return acc;
-  }, new Set());
-  return Array.from(categoriesCollection);
-};
-
 describe(`API returns categories`, () => {
-  const categories = getAllCategories(mockData);
+  const categories = [
+    `Архитектура`,
+    `Музыка`,
+    `Шапито`,
+    `Железо`,
+    `IT`,
+    `Программирование`,
+    `Разное`,
+    `Без рамки`,
+    `Живопись`,
+    `За жизнь`,
+    `Кино`,
+    `Деревья`
+  ];
 
   test(`Should has "200 OK" status`, async () => {
     const response = await request(app)
