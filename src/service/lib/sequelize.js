e81557110e1190ed7ee940037b1757aaaf1819a3
Ilnaz <akpambekovilnaz@gmail.com>
2021-12-06 22:04:45 +0300

    readability fix

--- src/service/lib/sequelize.js
+++ src/service/lib/sequelize.js
@@ -11,10 +11,14 @@ if (somethingIsNotDefined) {
 }
 
 module.exports = new Sequelize(
-    DB_NAME, DB_USER, DB_PASSWORD, {
+    DB_NAME,
+    DB_USER,
+    DB_PASSWORD,
+    {
       host: DB_HOST,
       port: DB_PORT,
       dialect: `postgres`,
+      logging: console.log,
       pool: {
         max: 5,
         min: 0,
